18e056339492c8dde9ae53aafa9d53d16d3bb455
David Vierra <codewarrior@hawaii.rr.com>
2015-05-08 10:54:41 -1000

    Select Block is no longer undoable

--- src/mcedit2/editortools/select_block.py
+++ src/mcedit2/editortools/select_block.py
@@ -12,22 +12,6 @@ from mcedit2.util.load_ui import load_ui
 
 log = logging.getLogger(__name__)
 
-
-class SelectBlockCommand(QtGui.QUndoCommand):
-    def __init__(self, tool, mousePos, *args, **kwargs):
-        QtGui.QUndoCommand.__init__(self, *args, **kwargs)
-        self.setText("Select Block")
-        self.mousePos = mousePos
-        self.tool = tool
-
-    def undo(self):
-        self.tool.setMousePos(self.ray)
-
-    def redo(self):
-        self.previousPos = self.tool.mousePos
-        self.tool.setMousePos(self.mousePos)
-
-
 class SelectBlockTool(EditorTool):
     name = "Select Block"
     iconName = "edit_block"
@@ -46,8 +30,7 @@ class SelectBlockTool(EditorTool):
         self.toolWidget = load_ui("editortools/select_block.ui")
 
     def mousePress(self, event):
-        command = SelectBlockCommand(self, event.blockPosition)
-        self.editorSession.pushCommand(command)
+        self.setMousePos(event.blockPosition)
 
     def setMousePos(self, pos):
         self.mousePos = pos
