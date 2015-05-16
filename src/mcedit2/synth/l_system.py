3f245f0a4848d1a863feb99fda4b0c7b25c1bf8d
David Vierra <codewarrior@hawaii.rr.com>
2015-05-15 20:58:19 -1000

    Revise the introduction to L-systems

--- src/mcedit2/synth/l_system.py
+++ src/mcedit2/synth/l_system.py
@@ -15,61 +15,74 @@ from mceditlib.selection import BoundingBox
 log = logging.getLogger(__name__)
 
 """
+An L-system is a way of generating graphics and geometric structures through code. It is also
+a way of thinking about algorithmic generation.
 
-From Wikipedia:
-[An L-system or Lindenmayer system is a parallel rewriting system and
-a type of formal grammar. An L-system consists of an alphabet of symbols
-that can be used to make strings, a collection of production rules that
-expand each symbol into some larger string of symbols, an initial
-"axiom" string from which to begin construction, and a mechanism for
-translating the generated strings into geometric structures.]
+This is a very simple L-system that only renders a triangle with fixed coordinates:
 
-Briefly, an L-system allows you to start with a root symbol (the
-"axiom"), and then define rules for replacing that symbol with a number
-of sub-symbols. The rules may be applied repeatedly to add more
-symbols and more depth to the symbol list. The final element list is then
-translated into a form that can be displayed on-screen.
+    class Triangle(Symbol):
+        def replace():
+            p1 = (0, 0, 20)
+            p2 = (10, 0, -10)
+            p3 = (-10, 0, -10)
+            return [Line(p1, p2, blocktype="stone"),
+                    Line(p2, p3, blocktype="stone"),
+                    Line(p3, p1, blocktype="stone")]
 
-The Wiki page describes translating the symbols into Turtle Graphics commands
-from LOGO. Other forms such as OpenGL rendering commands or 2D painting
-commands from DirectDraw/QuickDraw/Cocoa2D are also possible, but typically
-the replacement rules must be designed with the output in mind, such as
-by using a parametric grammar as described further down the page.
+    initial_system = [Triangle()]
+    # use initial_system in an LSystemPlugin, etc...
 
-The original use of L-systems was to model organic growth, by using a
-set of replacement rules that results in symbols that can be replaced
-further. The rules are typically applied for a fixed number of
-iterations. For our purpose of modeling deliberate constructions it is
-more useful to have non-recursive replacement rules that can be run
-indefinitely because they will terminate when all of the elements have
-no replacements available.
+At the core, an L-system is made of one type of object, a Symbol, and two functions: a `replace`
+function and a `render` function. The `replace` function's job is to add increasing amounts of
+detail to the system by replacing a symbol with its components, and the `render` function is for
+transforming the final result from a list of Symbols into a form suitable for rendering. In our
+case, we have two kinds of `render` functions - one for producing OpenGL graphics to show on
+screen, and one for producing Minecraft blocks to either show on screen or write to the world file.
 
-The resulting tree of elements is then rendered into geometric points.
+An L-system as a whole is a simple list of Symbols. The initial state of the system is a list
+containing a single Symbol (called the "axiom" in the literature). The system is evolved to the
+next state by calling `replace` on each symbol in the list, and replacing it with the result of
+the replace function. Thus, the system is evolved by calling `replace` repeatedly, over a given
+number of iterations, or until no further symbols can be replaced.
 
-This file defines a variant of the L-system meant for creating geometric
-structures in 3D space. The alphabet of symbols is extended to include
-"parametrized" symbols, where a single symbol can take many forms according
-to parameters such as length, width, radius, etcetera. The production rules
-are also extended to consider the parameters of a symbol when formulating its
-replacements.
+A Symbol is an object that contains any number of parameters, and implements either the `replace`
+function, the `render` function, or both.
 
-The final step of translating the symbols into geometric structures will
-result in a list of coordinates and blocktypes, suitable for passing to
-the `setBlock` function of a Minecraft world or world editor.
+The `replace` function returns a list of new Symbols to replace this symbol in the system's list.
+Because Symbols are allowed to have parameters, the replace function can return different lists of
+new Symbols depending on the current Symbol's parameters. If the Symbol does not implement replace,
+it will remain in the system as-is during further iterations. *
 
-Alternately, the symbols can be rendered as OpenGL scene nodes instead of
-Minecraft blocks. This is usually much faster and is suitable for returning
-preview nodes from a Generate Tool plugin.
+The `renderBlocks` function returns a list of `setBlock` tuples: `(x, y, z, blockType)`.
+(in the future, `renderBlocks` may be changed to take the currently edited world as an argument
+so it can directly edit the world.) *
 
-Several default symbols are available for primitives such as lines and blocks.
-These provide both Minecraft block renderers and OpenGL scene node renderers.
-The block renderers will use only a single block type. For primitives filled with
-e.g. patterns of blocks, subclass them and override render()
+The `renderSceneNodes` function returns a list of `scenegraph.Node` objects to be rendered in an
+OpenGL viewport. *
 
-It may be possible to add different rendering outputs such as SVG or HTML.
-This is as simple as adding another render function to each symbol type, and
-calling that function on each symbol in the list. (See the renderBlocks and
-renderSceneNodes functions - they are really nothing more than `flatmap(f, symbol_list)`)
+If a Symbol implements both `replace` and `render` functions, it is said to be recursive. This
+means a system containing this symbol can be evolved for any number of iterations and will not
+ever "halt" with a list of non-replaceable symbols.
+
+To implement your own L-system, start by creating a Symbol subclass for your initial symbol.
+Implement `replace` on this symbol to return a list of symbols that will replace this symbol
+in the first iteration. You may create additional Symbol subclasses for the symbols that will
+replace the initial symbol. Optionally, implement `renderBlocks` and possibly `renderSceneNodes` on
+any symbols that can be rendered as the final output of the system.
+
+For generating Minecraft objects, it is useful for each Symbol to represent a region of 3D space,
+and for each `replace` function to subdivide this symbol into smaller regions represented by
+other Symbols. A `Geometric` symbol class is provided for this purpose that acts as an
+axis-aligned bounding box. `Geometric` also implements `renderSceneNodes` to help visualize the
+evolution of a system.
+
+A small collection of predefined symbols that implement both `render` functions are provided:
+Line, Fill, (TODO: more symbols), and others. You can return these from your Symbol's `replace` to
+render the final output of a system. These symbols usually have points or boxes as parameters, plus
+a `blocktype` parameter. They can save you the trouble of implementing any `render` functions.
+
+* `replace` and `render` may be implemented as generators using the `yield` statement instead of
+directly returning a `list`.
 """
 
 
@@ -259,3 +272,61 @@ class Line(Symbol):
         node = VertexNode([vertexArray])  # xxx LineNode
 
         return [node]
+
+"""
+
+From Wikipedia:
+[An L-system or Lindenmayer system is a parallel rewriting system and
+a type of formal grammar. An L-system consists of an alphabet of symbols
+that can be used to make strings, a collection of production rules that
+expand each symbol into some larger string of symbols, an initial
+"axiom" string from which to begin construction, and a mechanism for
+translating the generated strings into geometric structures.]
+
+Briefly, an L-system allows you to start with a root symbol (the
+"axiom"), and then define rules for replacing that symbol with a number
+of sub-symbols. The rules may be applied repeatedly to add more
+symbols and more depth to the symbol list. The final element list is then
+translated into a form that can be displayed on-screen.
+
+The Wiki page describes translating the symbols into Turtle Graphics commands
+from LOGO. Other forms such as OpenGL rendering commands or 2D painting
+commands from DirectDraw/QuickDraw/Cocoa2D are also possible, but typically
+the replacement rules must be designed with the output in mind, such as
+by using a parametric grammar as described further down the page.
+
+The original use of L-systems was to model organic growth, by using a
+set of replacement rules that results in symbols that can be replaced
+further. The rules are typically applied for a fixed number of
+iterations. For our purpose of modeling deliberate constructions it is
+more useful to have non-recursive replacement rules that can be run
+indefinitely because they will terminate when all of the elements have
+no replacements available.
+
+The resulting tree of elements is then rendered into geometric points.
+
+This file defines a variant of the L-system meant for creating geometric
+structures in 3D space. The alphabet of symbols is extended to include
+"parametrized" symbols, where a single symbol can take many forms according
+to parameters such as length, width, radius, etcetera. The production rules
+are also extended to consider the parameters of a symbol when formulating its
+replacements.
+
+The final step of translating the symbols into geometric structures will
+result in a list of coordinates and blocktypes, suitable for passing to
+the `setBlock` function of a Minecraft world or world editor.
+
+Alternately, the symbols can be rendered as OpenGL scene nodes instead of
+Minecraft blocks. This is usually much faster and is suitable for returning
+preview nodes from a Generate Tool plugin.
+
+Several default symbols are available for primitives such as lines and blocks.
+These provide both Minecraft block renderers and OpenGL scene node renderers.
+The block renderers will use only a single block type. For primitives filled with
+e.g. patterns of blocks, subclass them and override render()
+
+It may be possible to add different rendering outputs such as SVG or HTML.
+This is as simple as adding another render function to each symbol type, and
+calling that function on each symbol in the list. (See the renderBlocks and
+renderSceneNodes functions - they are really nothing more than `flatmap(f, symbol_list)`)
+"""
