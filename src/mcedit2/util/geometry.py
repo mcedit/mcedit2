9599097772b2a451f7d521eb11c3a8bba6fcb23b
David Vierra <codewarrior@hawaii.rr.com>
2015-05-24 05:37:03 -1000

    Remove unused mcedit2/util/geometry.py

--- src/mcedit2/util/geometry.py
+++
@@ -1,8 +0,0 @@
-"""
-    ${NAME}
-"""
-from __future__ import absolute_import, division, print_function, unicode_literals
-from collections import namedtuple
-import logging
-log = logging.getLogger(__name__)
-
