b12d4ff50e8453268afde81afd624400c4bc3d6d
David Vierra <codewarrior@hawaii.rr.com>
2015-06-01 22:51:37 -1000

    Rework createSelectionMask
    Move createSelectionMask into ShapedSelection.box_mask
    Remove "hollow" and "chance" args - use composed selections for this instead
    Don't recenter coordinates in box_mask
    Recenter coordinates in SphereShape instead
    DiamondShape is now broken.

--- src/mceditlib/selection/__init__.py
+++ src/mceditlib/selection/__init__.py
@@ -277,69 +277,7 @@ def rayIntersectsBox(box, ray):
 
     byDistance.sort(key=lambda a: a[0])
 
-    return [a[1:] for a in byDistance]
-
-
-def createSelectionMask(brushBox, shapeFunc, requestedBox, chance=100, hollow=False):
-    """
-    Return a boolean array for a selection with the given shape and style.
-    If 'offset' and 'box' are given, then the selection is offset into the world
-    and only the part of the world contained in box is returned as an array
-    """
-
-    origin, shape = brushBox.origin, brushBox.size
-
-    if chance < 100 or hollow:
-        requestedBox = requestedBox.expand(1)
-
-    # we are returning indices for a Blocks array, so swap axes to YZX
-    outputShape = requestedBox.size
-    outputShape = (outputShape[1], outputShape[2], outputShape[0])
-
-    shape = shape[1], shape[2], shape[0]
-    origin = numpy.array(origin) - numpy.array(requestedBox.origin)
-    origin = origin[[1, 2, 0]]
-
-    inds = numpy.indices(outputShape, dtype=numpy.float32)
-    halfshape = numpy.array([(i >> 1) - ((i & 1 == 0) and 0.5 or 0) for i in shape])
-
-    blockCenters = inds - halfshape[:, None, None, None]
-    blockCenters -= origin[:, None, None, None]
-
-    # odd diameter means measure from the center of the block at 0,0,0 to each block center
-    # even diameter means measure from the 0,0,0 grid point to each block center
-
-    # if diameter & 1 == 0: blockCenters += 0.5
-    shape = numpy.array(shape, dtype='float32')
-
-    mask = shapeFunc(blockCenters, shape)
-
-    if (chance < 100 or hollow) and max(shape) > 1:
-        threshold = chance / 100.0
-        exposedBlockMask = numpy.ones(shape=outputShape, dtype='bool')
-        exposedBlockMask[:] = mask
-        submask = mask[1:-1, 1:-1, 1:-1]
-        exposedBlockSubMask = exposedBlockMask[1:-1, 1:-1, 1:-1]
-        exposedBlockSubMask[:] = False
-
-        for dim in (0, 1, 2):
-            slices = [slice(1, -1), slice(1, -1), slice(1, -1)]
-            slices[dim] = slice(None, -2)
-            exposedBlockSubMask |= (submask & (mask[slices] != submask))
-            slices[dim] = slice(2, None)
-            exposedBlockSubMask |= (submask & (mask[slices] != submask))
-
-        if hollow:
-            mask[~exposedBlockMask] = False
-        if chance < 100:
-            rmask = numpy.random.random(mask.shape) < threshold
-
-            mask[exposedBlockMask] = rmask[exposedBlockMask]
-
-    if chance < 100 or hollow:
-        return mask[1:-1, 1:-1, 1:-1]
-    else:
-        return mask
+    return [d[1:] for d in byDistance]
 
 
 class BoundingBox(SelectionBox):
@@ -600,19 +538,62 @@ class FloatBox(BoundingBox):
 
 
 class ShapedSelection(BoundingBox):
-    def __init__(self, box, shapeFunc, chance=100, hollow=False):
+    def __init__(self, box, shapeFunc):
         """
+        Generic class for implementing shaped selections via a shapeFunc callable.
 
-        :type shapeFunc: Style
+        shapeFunc is called with two arguments:
+
+            blockPositions: Three parallel arrays of coordinates (y, z, x). All arrays have the same
+                shape, which may reflect the size of the box requested in calls to box_mask. These
+                coordinates are relative to the ShapedSelection's bounding box.
+
+                xxx pass coordinates in world space for things like noise functions??
+
+            selectionShape: The size of this ShapedSelection (y, z, x)
+
+                xxx pass the ShapedSelection itself?
+
+        shapeFunc should return a boolean array with shape equal to the shape of the arrays in
+        blockPositions.
+
+        # xxx this init is not compatible with BoundingBox.__init__ and can't intersect
+
+        :type shapeFunc: Callable(blockPositions, selectionShape)
         :type box: BoundingBox
         """
         super(ShapedSelection, self).__init__(box.origin, box.size)
         self.shapeFunc = shapeFunc
-        self.chance = chance
-        self.hollow = hollow
 
     def box_mask(self, box):
-        return createSelectionMask(self, self.shapeFunc, box, self.chance, self.hollow)
+        """
+
+        :param box: Area to return a mask array for, in world coordinates
+        :type box: BoundingBox
+        :return: numpy.ndarray[ndim=3,dtype=bool]
+        """
+        origin, shape = self.origin, self.size
+
+        # we are returning indices for a Blocks array, so swap axes to YZX
+        outputShape = box.size
+        outputShape = (outputShape[1], outputShape[2], outputShape[0])
+
+        shape = shape[1], shape[2], shape[0]
+
+        # find requested box's coordinates relative to selection
+        offset = numpy.array(box.origin) - numpy.array(origin)
+        offset = offset[[1, 2, 0]]
+
+        # create coordinate array
+        blockPositions = numpy.indices(outputShape, dtype=numpy.float32)
+
+        # offset by requested box's origin
+        blockPositions += offset[:, None, None, None]
+
+        shape = numpy.array(shape, dtype='float32')
+
+        mask = self.shapeFunc(blockPositions, shape)
+        return mask
 
     def __cmp__(self, b):
         return cmp((self.origin, self.size, self.shapeFunc), None if b is None else (b.origin, b.size, b.shapeFunc))
@@ -633,27 +614,42 @@ class ShapedSelection(BoundingBox):
 # --- Shape functions ---
 
 
-def SphereShape(blockCenters, shape):
-    blockCenters *= blockCenters
-    shape /= 2
-    shape *= shape
+def SphereShape(blockPositions, shape):
+    # x^2 + y^2 + z^2 < r^2
+    #
+    # blockPositions are the positions of the lower left corners of each block.
+    #
+    # to define the sphere, we measure the distance from the center of each block
+    # to the sphere's center, which will be on a block edge when the size is even
+    # or at a block center when the size is odd.
+    #
+    # to this end, we offset blockPositions downward so the sphere's center is at 0, 0, 0
+    # and blockPositions are the positions of the centers of the blocks
 
-    blockCenters /= shape[:, None, None, None]
-    distances = sum(blockCenters, 0)
+    radius = shape / 2.0
+    offset = radius - 0.5
+
+    blockPositions -= offset[:, None, None, None]
+
+    blockPositions *= blockPositions
+    radius2 = radius * radius
+
+    blockPositions /= radius2[:, None, None, None]
+    distances = sum(blockPositions, 0)
     return distances < 1
 
 
-def BoxShape(blockCenters, shape):
-    blockCenters /= shape[:, None, None, None]  # XXXXXX USING DIVIDE FOR A RECTANGLE
+def BoxShape(blockPositions, shape):
+    blockPositions /= shape[:, None, None, None]  # XXXXXX USING DIVIDE FOR A RECTANGLE
 
-    distances = numpy.absolute(blockCenters).max(0)
+    distances = numpy.absolute(blockPositions).max(0)
     return distances < .5
 
 
-def DiamondShape(blockCenters, shape):
-    blockCenters = numpy.abs(blockCenters)
+def DiamondShape(blockPositions, shape):
+    blockPositions = numpy.abs(blockPositions)
     shape /= 2
-    blockCenters /= shape[:, None, None, None]
-    distances = numpy.sum(blockCenters, 0)
+    blockPositions /= shape[:, None, None, None]
+    distances = numpy.sum(blockPositions, 0)
     return distances < 1
 
