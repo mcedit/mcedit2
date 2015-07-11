844049b0d4aecb25fc480fae37111e8aebac6438
David Vierra <codewarrior@hawaii.rr.com>
2015-07-11 11:33:24 -1000

    Add mimeformats.py to support drag and drop

---
+++ src/mcedit2/util/mimeformats.py
@@ -0,0 +1,10 @@
+"""
+    mimeformats
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+import logging
+
+log = logging.getLogger(__name__)
+
+class MimeFormats(object):
+    MapItem = "application/x-mcedit-mapitem"
