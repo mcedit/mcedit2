31b411b75a2e875ecfd8f11e164bbc331298848e
David Vierra <codewarrior@hawaii.rr.com>
2015-09-05 10:43:23 -1000

    SpinSlider now accepts floating-point values (mainly those between 0.0 and 1.0)

--- src/mcedit2/widgets/spinslider.py
+++ src/mcedit2/widgets/spinslider.py
@@ -13,12 +13,27 @@ log = logging.getLogger(__name__)
 
 @registerCustomWidget
 class SpinSlider(QtGui.QWidget):
+
     def __init__(self, *args, **kwargs):
+        isDouble = kwargs.pop('double', False)
+        minimum = kwargs.pop('minimum', None)
+        maximum = kwargs.pop('maximum', None)
+        value = kwargs.pop('value', None)
+
+        self.isDouble = isDouble
+
         super(SpinSlider, self).__init__(*args, **kwargs)
         self.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
 
-        self.spinBox = QtGui.QSpinBox()
+        if isDouble:
+            self.spinBox = QtGui.QDoubleSpinBox()
+            self.sliderFactor = 100.0
+        else:
+            self.spinBox = QtGui.QSpinBox()
+            self.sliderFactor = 1.0
+
         self.slider = QtGui.QSlider()
+
         self.slider.setOrientation(Qt.Horizontal)
         self._value = 0
 
@@ -30,12 +45,20 @@ class SpinSlider(QtGui.QWidget):
 
         self.setLayout(Row(self.spinBox, self.slider, margin=0))
 
+        if minimum is not None:
+            self.setMinimum(minimum)
+        if maximum is not None:
+            self.setMaximum(maximum)
+        if value is not None:
+            self.setValue(value)
+
     def spinBoxChanged(self, value):
         self._value = value
-        self.slider.setValue(value)
+        self.slider.setValue(value * self.sliderFactor)
         self.valueChanged.emit(value)
 
     def sliderChanged(self, value):
+        value /= self.sliderFactor
         self._value = value
         self.spinBox.setValue(value)
         self.valueChanged.emit(value)
@@ -46,14 +69,14 @@ class SpinSlider(QtGui.QWidget):
     def setValue(self, value):
         self._value = value
         self.spinBox.setValue(value)
-        self.slider.setValue(value)
+        self.slider.setValue(value * self.sliderFactor)
 
     def minimum(self):
         return self._minimum
 
     def setMinimum(self, value):
         self._minimum = value
-        self.slider.setMinimum(value)
+        self.slider.setMinimum(value * self.sliderFactor)
         self.spinBox.setMinimum(value)
 
     def maximum(self):
@@ -61,7 +84,7 @@ class SpinSlider(QtGui.QWidget):
 
     def setMaximum(self, value):
         self._maximum = value
-        self.slider.setMaximum(value)
+        self.slider.setMaximum(value * self.sliderFactor)
         self.spinBox.setMaximum(value)
 
 
