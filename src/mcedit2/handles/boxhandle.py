dd195c34e8d53b48cf6e5bba45828a13f8dce5c5
David Vierra <codewarrior@hawaii.rr.com>
2015-10-01 17:10:18 -1000

    Refactor BoxHandle in preparation of making it movable.

--- src/mcedit2/handles/boxhandle.py
+++ src/mcedit2/handles/boxhandle.py
@@ -4,7 +4,7 @@
 from __future__ import absolute_import, division, print_function
 import logging
 
-from PySide import QtCore
+from PySide import QtCore, QtGui
 import numpy
 
 from mcedit2.rendering.scenegraph import scenenode
@@ -31,10 +31,25 @@ class BoxHandle(scenenode.Node, QtCore.QObject):
 
     hiliteFace = True
 
+    _resizable = True
+    modifier = QtCore.Qt.ShiftModifier
+    isMoving = False
+
     def __init__(self):
         """
-        A drawable, resizable box that can respond to mouse events. Emits boundsChanged whenever its bounding box
-        changes, and emits boundsChangedDone when the mouse button is released.
+        A drawable, resizable box that can respond to mouse events. Emits boundsChanged
+        whenever its bounding box changes, and emits boundsChangedDone when the
+        mouse button is released.
+
+        The handle initially has no box; the first mouse action will define
+        the initial box with height=0. Subsequent mouse actions will move or resize
+        the box.
+
+        Mouse events must be forwarded to the handle using mousePress, mouseDrag, and
+        mouseRelease.
+
+        A modifier can be set (default Shift) to move the box instead of resizing it;
+        or the `resizable` attribute can be set to False to always move the box.
         :return:
         :rtype:
         """
@@ -59,11 +74,24 @@ class BoxHandle(scenenode.Node, QtCore.QObject):
             self.faceDragNode.selectionBox = box
             self.boundsChanged.emit(box)
 
-    def dragResizePoint(self, ray):
-        # returns a point representing the intersection between the mouse ray
-        # and an imaginary plane perpendicular to the dragged face
+    def moveModifierDown(self, event):
+        return event.modifiers() & self.modifier
 
+    @property
+    def resizable(self):
+        return self._resizable
+
+    @resizable.setter
+    def resizable(self, value):
+        assert value or self.bounds, "Sanity check: Non-resizable BoxHandle must have bounds."
+        self._resizable = value
+
+    # --- Resize helpers ---
+
+    def dragResizePoint(self, ray):
         """
+        Return a point representing the intersection between the mouse ray
+        and an imaginary plane perpendicular to the dragged face
 
         :type ray: Ray
         :rtype: Vector
@@ -119,77 +147,137 @@ class BoxHandle(scenenode.Node, QtCore.QObject):
 
         return startBox.union(endBox)
 
-    def mousePress(self, event):
+    # --- Resize ---
 
-        # Find side of existing selection to drag
-        # xxxx can't do this with disjoint selections?
-        if self.bounds is not None:
-            point, face = boxFaceUnderCursor(self.bounds, event.ray)
+    def beginResize(self, event):
+        point, face = boxFaceUnderCursor(self.bounds, event.ray)
 
-            if face is not None:
-                log.info("Beginning drag resize")
-                self.dragResizeFace = face
-                # Choose a dimension perpendicular to the dragged face
-                # Try not to pick a dimension close to edge-on with the view vector
-                dim = ((face.dimension + 1) % 3)
+        if face is not None:
+            log.info("Beginning drag resize")
+            self.dragResizeFace = face
+            # Choose a dimension perpendicular to the dragged face
+            # Try not to pick a dimension close to edge-on with the view vector
+            dim = ((face.dimension + 1) % 3)
 
-                dim1 = (dim+1) % 3
+            dim1 = (dim+1) % 3
 
-                vector = event.view.cameraVector.abs()
-                if vector[dim1] > vector[dim]:
-                    dim = dim1
+            vector = event.view.cameraVector.abs()
+            if vector[dim1] > vector[dim]:
+                dim = dim1
 
-                self.dragResizeDimension = dim
-                self.dragResizePosition = point[self.dragResizeDimension]
-                self.oldBounds = self.bounds
-                return True
-            else:
-                # Didn't hit - start new selection
-                self.bounds = None
+            self.dragResizeDimension = dim
+            self.dragResizePosition = point[self.dragResizeDimension]
+            self.oldBounds = self.bounds
+            return True
+        else:
+            # Didn't hit - start new selection
+            self.bounds = None
 
-        # Get ready to start new selection
+    def continueResize(self, event):
+        # Hilite face being dragged
+        newBox = self.boxFromDragResize(self.oldBounds, event.ray)
+        self.faceDragNode.selectionBox = newBox
+        self.faceDragNode.visible = True
+
+        # Update selection box to resized size in progress
+        newBox = self.boxFromDragResize(self.oldBounds, event.ray)
+        self.bounds = newBox
+
+    def endResize(self, event):
+        self.bounds = self.boxFromDragResize(self.oldBounds, event.ray)
+        self.oldBounds = None
+        self.dragResizeFace = None
+        self.faceDragNode.visible = False
+        self.boundsChangedDone.emit(self.bounds, False)
+
+    # --- Create ---
+
+    def beginCreate(self, event):
+        self.dragStartPoint = event.blockPosition
+        self.dragStartFace = faces.FaceYIncreasing  # event.blockFace
+
+    def continueCreate(self, event):
+        # Show new box being dragged out
+        newBox = self.boxFromDragSelect(event.ray)
+        self.bounds = newBox
+
+    def endCreate(self, event):
+        newBox = self.boxFromDragSelect(event.ray)
+        self.dragStartPoint = None
+        self.bounds = newBox
+        self.boundsChangedDone.emit(newBox, True)
+
+    # --- Move ---
+
+    def beginMove(self, event):
+        pass
+
+    def continueMove(self, event):
+        pass
+
+    def endMove(self, event):
+        pass
+
+    # --- Update ---
+
+    def updateMouseHover(self, event):
+        """
+        Update visuals for the mouse hovering over this box.
+        """
+        # Hilite face cursor is over
         if self.bounds is None:
-            self.dragStartPoint = event.blockPosition
-            self.dragStartFace = faces.FaceYIncreasing  # event.blockFace
             return
+        point, face = boxFaceUnderCursor(self.bounds, event.ray)
+        if face is not None:
+            self.faceDragNode.visible = True
+            self.faceDragNode.face = face
+            self.faceDragNode.selectionBox = self.bounds
+        else:
+            self.faceDragNode.visible = False
+        if self.moveModifierDown(event):
+            self.faceDragNode.color = self._moveFaceColor
+        else:
+            self.faceDragNode.color = self._resizeFaceColor
+
+    _moveFaceColor = (0.3, 0.9, 0.3)
+    _resizeFaceColor = (0.3, 0.6, 0.9)
+
+    # --- Mouse events ---
+
+    def mousePress(self, event):
+        if self.resizable and not self.moveModifierDown(event):
+            # Find side of existing selection to drag
+            # xxxx can't do this with disjoint selections?
+            if self.bounds is not None:
+                self.beginResize(event)
+
+            # Get ready to start new selection
+            if self.bounds is None:
+                self.beginCreate(event)
+        else:
+            self.beginMove(event)
 
     def mouseMove(self, event):
-        if self.dragStartPoint:
-            # Show new box being dragged out
-            newBox = self.boxFromDragSelect(event.ray)
-            self.bounds = newBox
+        # Called whether or not the mouse button is held.
+        if self.resizable and not self.moveModifierDown(event):
+            if self.dragStartPoint:
+                self.continueCreate(event)
 
-        if self.bounds is not None:
-            if self.dragResizeFace is not None:
-                # Hilite face being dragged
-                newBox = self.boxFromDragResize(self.oldBounds, event.ray)
-                self.faceDragNode.selectionBox = newBox
-                self.faceDragNode.visible = True
+            elif self.bounds is not None:
+                if self.dragResizeFace is not None:
+                    self.continueResize(event)
+        else:
+            self.continueMove(event)
 
-                # Update selection box to resized size in progress
-                newBox = self.boxFromDragResize(self.oldBounds, event.ray)
-                self.bounds = newBox
-
-            elif self.hiliteFace:
-                # Hilite face cursor is over
-                point, face = boxFaceUnderCursor(self.bounds, event.ray)
-                if face is not None:
-                    self.faceDragNode.visible = True
-                    self.faceDragNode.face = face
-                    self.faceDragNode.selectionBox = self.bounds
-                else:
-                    self.faceDragNode.visible = False
+        if self.hiliteFace:
+            self.updateMouseHover(event)
 
     def mouseRelease(self, event):
-        if self.dragStartPoint:
-            newBox = self.boxFromDragSelect(event.ray)
-            self.dragStartPoint = None
-            self.bounds = newBox
-            self.boundsChangedDone.emit(newBox, True)
+        if self.resizable and not self.moveModifierDown(event):
+            if self.dragStartPoint:
+                self.endCreate(event)
 
-        elif self.dragResizeFace is not None:
-            self.bounds = self.boxFromDragResize(self.oldBounds, event.ray)
-            self.oldBounds = None
-            self.dragResizeFace = None
-            self.faceDragNode.visible = False
-            self.boundsChangedDone.emit(self.bounds, False)
+            elif self.dragResizeFace is not None:
+                self.endResize(event)
+        else:
+            self.endMove(event)
