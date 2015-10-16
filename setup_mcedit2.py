8d02b6df4b5fab3bf7e514aa913c08342574dc96
David Vierra <codewarrior@hawaii.rr.com>
2015-10-16 12:44:18 -1000

    Split setup.py into two files

---
+++ setup_mcedit2.py
@@ -0,0 +1,61 @@
+"""
+    setup-mcedit2
+"""
+
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy
+
+# Output annotated .html
+import Cython.Compiler.Options
+Cython.Compiler.Options.annotate = True
+
+
+with file("version.txt") as f:
+    version = f.read().strip()
+
+install_requires = [
+    "numpy",
+]
+
+mcedit2_ext_modules = cythonize(
+    [
+        "src/mcedit2/rendering/blockmodels.pyx",
+        "src/mcedit2/rendering/modelmesh.pyx",
+    ]
+    )
+
+setup(name='mcedit2',
+      version=version,
+      description="Interactive 3D World Editor for Minecraft Levels",
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "Environment :: Win32 (MS Windows)",
+          "Environment :: X11 Applications :: Qt",
+          "Environment :: MacOS X",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+      ],
+      keywords='minecraft',
+      author='David Vierra',
+      author_email='codewarrior0@gmail.com',
+      url='https://github.com/mcedit/mcedit2',
+      license='MIT License',
+      packages=["mcedit2"],
+      package_dir={'': 'src'},
+      ext_modules=mcedit2_ext_modules,
+      include_dirs=numpy.get_include(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      mcedit2=mcedit2.main:main
+      """,
+      )
