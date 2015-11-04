ea88d7b36e6cf104bcf736ba77cd7443bdf51d7b
David Vierra <codewarrior@hawaii.rr.com>
2015-11-04 13:23:14 -1000

    Finish fixing up test_relight

--- tests/test_mceditlib/relight_test.py
+++ tests/test_mceditlib/relight_test.py
@@ -6,13 +6,7 @@ import numpy
 def test_relight(schematic_world, pc_world):
     anvilDim = pc_world.getDimension()
     bounds = anvilDim.bounds
-    point = bounds.origin + (bounds.size * (0.5, 0.5, 0.5))
-
-    box = bounds.expand(-100, 0, -100)
-
-    chunks = [(cx, cz) for cx, cz in anvilDim.chunkPositions() if (cx << 4, 1, cz << 4) not in box]
-    for c in chunks:
-        anvilDim.deleteChunk(*c)
+    point = bounds.origin + (bounds.size * (0.5, 0.25, 0.5))
 
     stationDim = schematic_world.getDimension()
     anvilDim.copyBlocks(stationDim, stationDim.bounds, point, create=True)
@@ -22,9 +16,14 @@ def test_relight(schematic_world, pc_world):
     cz = int(point.z + 32) >> 4
 
     def check():
-        sl = numpy.sum(anvilDim.getChunk(cx, cz).SkyLight)
-        bl = numpy.sum(anvilDim.getChunk(cx, cz).BlockLight)
-        assert (sl, bl) == (341328, 43213)
+        sl = 0
+        bl = 0
+        chunk = anvilDim.getChunk(cx, cz)
+        for cy in chunk.sectionPositions():
+            section = chunk.getSection(cy)
+            sl += numpy.sum(section.SkyLight)
+            bl += numpy.sum(section.BlockLight)
+        assert (sl, bl) == (367965, 48261)
 
     check()
 
