55b0dc9aae47128eed00b9aff71c991cf8855fbd
David Vierra <codewarrior@hawaii.rr.com>
2016-06-26 12:15:09 -1000

    Add sphinx.ext.napoleon to read NumPyDoc docstrings

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.abspath('extensions'))  # extensions
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
