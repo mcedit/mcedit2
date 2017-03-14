b5d0553129489fdf7dbfe70bcf349133b79ac0bc
David Vierra <codewarrior@hawaii.rr.com>
2017-03-13 16:44:12 -1000

    Fix #295 - Cope with broken texture references in mod/respack models

--- src/mcedit2/rendering/blockmodels.pyx
+++ src/mcedit2/rendering/blockmodels.pyx
@@ -339,9 +339,7 @@ cdef class BlockModels(object):
 
         textureVars = {}
         for k, v in rawTextureVars.iteritems():
-            if v is None:
-                continue
-            while v.startswith("#"):
+            while v is not None and v.startswith("#"):
                 v = rawTextureVars.get(v[1:])
             if v:
                 textureVars[k] = v
